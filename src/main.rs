1a55c405aeb06ace0a614010e29405b2c41d565b
Dewan Ishtiaque Ahmed <30604461+dewan-ahmed@users.noreply.github.com>
2024-04-12 16:30:50 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,18 @@
 
 use qrcode::QrCode;
 use std::env;
-use std::path::Path;
+use std::fs::File;
+use std::io::Write;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let url = &args[1];
     let code = QrCode::new(url).unwrap();
     let image = code.render::<char>().quiet_zone(false).build();
-    let path = Path::new("qrcode.png");
-    image.save(path).expect("Failed to save image to output path");
+
+    let path = "qrcode.png";
+    let mut file = File::create(path).unwrap_or_else(|e| panic!("Failed to create file: {}", e));
+    file.write_all(image.as_bytes()).unwrap_or_else(|e| panic!("Failed to write image data: {}", e));
 }
 
 // $ cargo run https://www.rust-lang.org/
